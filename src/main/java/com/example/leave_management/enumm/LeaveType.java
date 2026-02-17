4db7a2a194bb9cdbffeedb97714dda97e0a33cb5
Abdelrahman <117858884+Abdelrhaman0@users.noreply.github.com>
2026-02-17 17:06:08 +0200

    Update LeaveType.java

--- src/main/java/com/example/leave_management/enumm/LeaveType.java
+++ src/main/java/com/example/leave_management/enumm/LeaveType.java
@@ -2,8 +2,18 @@ package com.example.leave_management.enumm;
 
 
 public enum LeaveType {
-    SICK,        // مرضية
-    CASUAL,      // اعتيادية
-    ANNUAL,      // سنوية
-    EMERGENCY    // طارئة
+    SICK("مرضية"),
+    CASUAL("اعتيادية"),
+    ANNUAL("سنوية"),
+    EMERGENCY("طارئة");
+
+    private final String displayName;
+
+    LeaveType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
